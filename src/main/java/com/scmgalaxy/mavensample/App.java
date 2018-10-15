3c7c5bbb12f265a38f07083c05717db9876aab8e
vishnunvv <43653471+vishnunvv@users.noreply.github.com>
2018-10-15 12:57:22 +0530

    for SP-2
    App.java has been updated

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World  MYP-2 RefX! MAH-2--d" );
+        System.out.println( "Hello world  MYP-2 RefX! MAH-2--d" );
     }
 }
